e811e0cc5369ae3609aee98fe770cd94a6753677
E-AlexGM <eagranadinomendoza@gmail.com>
2026-05-20 16:33:27 -0600

    refactor: Se han modificado todos los métodos de PreguntaResource, validaciones y códigos de estados para consistencia con los demás test

--- src/main/java/sv/edu/ues/occ/ingenieria/tpi135_2026/admision_api/webapp/core/boundary/rest/PreguntaResource.java
+++ src/main/java/sv/edu/ues/occ/ingenieria/tpi135_2026/admision_api/webapp/core/boundary/rest/PreguntaResource.java
@@ -23,83 +23,89 @@ import jakarta.ws.rs.core.UriInfo;
 import sv.edu.ues.occ.ingenieria.tpi135_2026.admision_api.webapp.core.control.PreguntaDAO;
 import sv.edu.ues.occ.ingenieria.tpi135_2026.admision_api.webapp.core.entity.Pregunta;
 
-@Path("pregunta")  
-public class PreguntaResource implements Serializable{
+@Path("pregunta")
+public class PreguntaResource implements Serializable {
 
     @Inject
-    PreguntaDAO pDAO;  
+    PreguntaDAO preguntaDAO;
 
     @POST
-    @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
-    public Response crear(Pregunta p, @Context UriInfo uriInfo){
-        if(p != null){
-            p.setIdPregunta(UUID.randomUUID());
-            pDAO.crear(p);
-            UriBuilder builder = uriInfo.getAbsolutePathBuilder();
-            builder.path(p.getIdPregunta().toString());
-            return Response.status(Response.Status.CREATED).location(builder.build()).build();
+    @Consumes({MediaType.APPLICATION_JSON})
+    public Response crear(Pregunta pregunta, @Context UriInfo uriInfo) {
+        if (pregunta != null && pregunta.getIdPregunta() == null) {
+            pregunta.setIdPregunta(UUID.randomUUID());
+            preguntaDAO.crear(pregunta);
+            UriBuilder uriBuilder = uriInfo.getAbsolutePathBuilder();
+            uriBuilder.path(pregunta.getIdPregunta().toString());
+            return Response.created(uriBuilder.build()).build();
         }
-        return Response.status(Response.Status.BAD_REQUEST).header(ResponseHeaders.WRONG_PARAMETER.toString(), "PREGUNTA").build();
+        return Response.status(Response.Status.BAD_REQUEST).header(ResponseHeaders.WRONG_PARAMETER.toString(), "El recurso no puede ser nulo y no debe tener un ID asignado").build();
     }
 
     @DELETE
     @Path("{id}")
-    public Response eliminar(@PathParam("id") UUID idPregunta){
-        if(idPregunta != null){
-            Pregunta p = pDAO.buscarPorId(idPregunta);
-            if(p != null){
-                pDAO.eliminar(p);
-                return Response.status(Response.Status.NO_CONTENT).build();
+    public Response eliminar(@PathParam("id") UUID id) {
+        if (id != null) {
+            Pregunta pregunta = preguntaDAO.buscarPorId(id);
+            if (pregunta != null) {
+                preguntaDAO.eliminar(pregunta);
+                return Response.noContent().build();
             }
-            return Response.status(Response.Status.NOT_FOUND).header(ResponseHeaders.NOT_FOUND.toString(), "PREGUNTA").build();
+            return Response.status(Response.Status.NOT_FOUND).header(ResponseHeaders.NOT_FOUND.toString(), "Recurso no encontrado").build();
         }
-        return Response.status(Response.Status.BAD_REQUEST).header(ResponseHeaders.WRONG_PARAMETER.toString(), "ID").build();
-        
+        return Response.status(Response.Status.BAD_REQUEST).header(ResponseHeaders.WRONG_PARAMETER.toString(), "El ID no puede ser nulo").build();
     }
-
-    @PUT
-    @Consumes({MediaType.APPLICATION_JSON})
-    @Path("{id}")
-    public Response actualizar(Pregunta p, @PathParam("id") UUID idPregunta){
-        if(p != null && idPregunta != null){
-            Pregunta pExistente = pDAO.buscarPorId(idPregunta);
-            if(pExistente != null){
-                p.setIdPregunta(idPregunta);
-                pDAO.actualizar(p);
-                return Response.status(Response.Status.OK).entity(p).build();
-            }
-            return Response.status(Response.Status.NOT_FOUND).header(ResponseHeaders.NOT_FOUND.toString(), "PREGUNTA").build();
-        }
-        return Response.status(Response.Status.BAD_REQUEST).header(ResponseHeaders.WRONG_PARAMETER.toString(), "ID o PREGUNTA").build();
-    }
-
-     @GET
-     @Path("{id}")
-     @Produces({MediaType.APPLICATION_JSON})
-     public Response buscarPorId(@PathParam("id") UUID idPregunta){
-         if(idPregunta != null){
-             Pregunta p = pDAO.buscarPorId(idPregunta);
-             if(p != null){
-                 return Response.status(Response.Status.OK).entity(p).build();
-             }
-             return Response.status(Response.Status.NOT_FOUND).header(ResponseHeaders.NOT_FOUND.toString(), "PREGUNTA").build();
-         }
-         return Response.status(Response.Status.BAD_REQUEST).header(ResponseHeaders.WRONG_PARAMETER.toString(), "ID").build();
-     }
 
     @GET
     @Produces({MediaType.APPLICATION_JSON})
-    public Response buscarPorRango(@QueryParam("first") @DefaultValue("0") int first, @QueryParam("size") @DefaultValue("50") int hasta){
-        if(first >= 0 && hasta > 0){
-            List<Pregunta> preguntas = pDAO.buscarPorRango(first, hasta);
-            Long total = pDAO.contar();
+    public Response buscarPorRango(
+            @QueryParam("first")
+            @DefaultValue("0")
+            int firstResult,
+            @QueryParam("max")
+            @DefaultValue("50")
+            int maxResults) {
+        if (firstResult >= 0 && maxResults > 0 && maxResults <= 50) {
+            List<Pregunta> preguntas = preguntaDAO.buscarPorRango(firstResult, maxResults);
+            Long total = preguntaDAO.contar();
             Response.ResponseBuilder responseBuilder = Response.ok(preguntas)
                     .header(ResponseHeaders.TOTAL_RECORDS.toString(), total)
                     .type(MediaType.APPLICATION_JSON);
             return responseBuilder.build();
         }
-        return Response.status(Response.Status.BAD_REQUEST).header(ResponseHeaders.WRONG_PARAMETER.toString(), "FIRST o SIZE").build();
+        return Response.status(Response.Status.BAD_REQUEST).header(ResponseHeaders.WRONG_PARAMETER.toString(), "Los parámetros 'first' debe ser >= 0 y 'max' debe ser > 0 y <= 50").build();
+    }
 
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response buscarPorId(@PathParam("id") UUID id) {
+        if (id != null) {
+            Pregunta encontrado = preguntaDAO.buscarPorId(id);
+            if (encontrado != null) {
+                Response.ResponseBuilder builder = Response.ok(encontrado).type(MediaType.APPLICATION_JSON);
+                return builder.build();
+            }
+            return Response.status(Response.Status.NOT_FOUND).header(ResponseHeaders.NOT_FOUND.toString(), "Recurso no encontrado").build();
+        }
+        return Response.status(Response.Status.BAD_REQUEST).header(ResponseHeaders.WRONG_PARAMETER.toString(), "id: " + id).build();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response actualizar(@PathParam("id") UUID id, Pregunta pregunta) {
+        if (pregunta != null && id != null) {
+            Pregunta existente = preguntaDAO.buscarPorId(id);
+            if (existente != null) {
+                pregunta.setIdPregunta(id);
+                preguntaDAO.actualizar(pregunta);
+                return Response.ok(pregunta).build();
+            }
+            return Response.status(Response.Status.NOT_FOUND).header(ResponseHeaders.NOT_FOUND.toString(), "Recurso no encontrado").build();
+        }
+        return Response.status(Response.Status.BAD_REQUEST).header(ResponseHeaders.WRONG_PARAMETER.toString(), "El recurso no puede ser nulo y debe tener un ID asignado").build();
     }
 }
